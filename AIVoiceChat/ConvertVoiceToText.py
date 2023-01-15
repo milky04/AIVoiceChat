b8855189e7b6414dce59e1ef0a9612d77cd76517
milky04 <76756065+milky04@users.noreply.github.com>
2023-01-16 00:25:48 +0900

    Update ConvertVoiceToText.py

--- AIVoiceChat/ConvertVoiceToText.py
+++ AIVoiceChat/ConvertVoiceToText.py
@@ -4,6 +4,11 @@ whisper_model = whisper.load_model("small")
 language = 'ja'
 
 def convert_voice_to_text(audio):
-    result = whisper_model.transcribe(audio, verbose=True, language=language)
+    result = whisper_model.transcribe(
+        audio,
+        verbose=True,
+        language=language,
+        fp16=False
+        )
     result_text = result['text']
     return result_text
